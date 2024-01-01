b05d34fc8dff283ed6554dfac5b8c9d15f38a22b
Sri Vaishnavi Bhaskara <vaishnavisri.2614@gmail.com>
2024-01-01 17:09:07 +0530

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,10 @@
+
+# Short project on full stack
+
+Submitting this mini project as assignment - 2 for II B.TECH I SEMESTER
+TECH STACK:
+    HTML
+    CSS3
+    JAVASCRIPT
+    NODEJS
+    SQL
---
+++ working.js
@@ -0,0 +1,16 @@
+const asideSection=document.querySelector("aside");
+const mainSection= document.querySelector("main");
+const signInBtn= document.getElementById("sign-in-btn");
+const signUpBtn= document.getElementById("sign-up-btn");
+signInBtn.addEventListener("click",()=>
+{
+mainSection.classList.add("slideRight");
+asideSection.classList.add("slideLeft");
+});
+signUpBtn.addEventListener("click",()=>
+{
+  mainSection.classList.remove("slideRight");
+  asideSection.classList.remove("slideLeft");
+});
+const showPasswordBtn = document.querySelector('.show-password');
+const passwordInput = document.getElementById('password');
